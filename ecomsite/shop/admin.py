aefac2d856ce851234b815f595f4326358590558
kulhas <hmrc_aim@hotmail.com>
2025-05-13 13:57:19 +0100

    ecom Admin changes

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -3,5 +3,23 @@ from .models import Products, Order
 
 
 # Register your models here.
-admin.site.register(Products)
+
+admin.site.site_header = "E-commerce Site"
+admin.site.site_title = "Power World"
+admin.site.index_title = "Manage Power World"
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def change_category_to_default(self,request, queryset):
+        queryset.update(category = "default")
+
+    change_category_to_default.short_description = 'Default Category'
+    list_display = ('title', 'price', 'discount_price', 'category', 'description',)
+    search_fields = ('title', 'category',)
+    actions = ('change_category_to_default',)
+    list_editable = ('price', 'discount_price')
+
+
+admin.site.register(Products, ProductAdmin)
 admin.site.register(Order)
